1e3c575c11023c55550d61d68170398987687c39
Bob Heineman <heineman.bob@gmail.com>
2016-11-17 19:00:35 -0600

    adjust for additional fields

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,8 @@ RUN apt-get -qy install curl ca-certificates python sysstat
 ADD ./get-pip.py .
 ADD ./logstash_test.py .
 RUN python get-pip.py 
+
 RUN pip install python-logstash
+RUN pip install psutil
+
 CMD ["python", "logstash_test.py"] 
--- logstash_test.py
+++ logstash_test.py
@@ -1,27 +1,24 @@
 import logging
 import logstash
 import sys
+import psutil
 
 host = '192.168.0.113'
 port = 5000
 
 test_logger = logging.getLogger('python-logstash-logger')
 test_logger.setLevel(logging.INFO)
-#test_logger.addHandler(logstash.LogstashHandler(host, 5959, version=1))
 test_logger.addHandler(logstash.TCPLogstashHandler(host, port, version=1))
 
-test_logger.error('python-logstash: test logstash error message.')
-test_logger.info('python-logstash: test logstash info message.')
-test_logger.warning('python-logstash: test logstash warning message.')
-
 # add extra field to logstash message
-extra = {
-    'test_string': 'python version: ' + repr(sys.version_info),
-    'test_boolean': True,
-    'test_dict': {'a': 1, 'b': 'c'},
-    'test_float': 1.23,
-    'test_integer': 123,
-    'test_list': [1, 2, '3'],
+info = {
+    'cpu_usage': psutil.cpu_percent(),
+    'memory_usage': psutil.virtual_memory().cpu_percent,
+    'memory_free': psutil.virtual_memory().free,
+    'memory_swap': psutil.swap_memory().percent,
+    'disk_space_used': psutil.disk_usage('/').percent,
+    'network_connections': len(psutil.net_connections()),
+    'network_connection_details': psutil.net_connections()
 }
 
-test_logger.info('python-logstash: test extra fields', extra=extra)
+test_logger.info('pi_stats', extra=info)
